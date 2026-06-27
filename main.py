d49e355ac1c7be9f9b121f1cbfd1f59f93ea9dde
thaiscrtn <thaiscrtn@gmail.com>
2026-06-27 18:03:59 +0100

    Fix: Fixes the translation dictionary and column handling

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from src.pipeline import process_data
 
 if __name__ == "__main__":
+    print('STARTING THE AUTOMATION ROBOT...\n')
+
+    print("-- PROCESSING CLIENTS --")
+    process_data('data/raw_data.csv', 'data/clean_data.csv')
+
+    print('\n-----------------------------------------------')
+
+    print("-- PROCESSING SALES")
+    process_data('data/data_sales.csv', 'data/clean_sales.csv')
+
     # Path th files
-    RAW_FILE = 'data/raw_data.csv'
-    CLEAN_FILE = 'data/clean_data.csv'
+    # RAW_FILE = 'data/raw_data.csv'
+    # CLEAN_FILE = 'data/clean_data.csv'
 
     # Run robo
-    process_data(RAW_FILE, CLEAN_FILE)
+   # process_data(RAW_FILE, CLEAN_FILE)
--- src/pipeline.py
+++ src/pipeline.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+from deep_translator import GoogleTranslator
 
 
 def process_data(entrance_path, exit_path):
@@ -19,17 +20,62 @@ def process_data(entrance_path, exit_path):
     # Cleaning Step
     print('Starting the data cleaning...')
 
-    # 1. Remove duplicates
+    # ==========================================================
+    # 1. GENERAL TREATMENTS - Using for any base
+    # ==========================================================
     lines_before = len(df)
     df = df.drop_duplicates()
     print(f".   - Removed {lines_before - len(df)} duplicates lines")
 
-    # 2. Treating null values
-    # Customize based on the database, but the generic example is: "Not provided"
-    df = df.fillna("Not provided")
+    # Remove extra whitespace from the beginning/end of text on the lines
+    for col in df.select_dtypes(include=['object']).columns:
+        df[col] = df[col].astype(str).str.strip()
 
-    # 3. Standardize text (lowercase columns, no extra spaces at the ends)
-    df.columns = df.columns.str.strip().str.lower()
+    # Standardize text (lowercase columns, no extra spaces at the ends)
+    df.columns = df.columns.str.strip().str.lower().str.replace(" ", "_")
+
+    # Dictionary for specific corrections (avoiding translation bugs)
+    corrections = {
+        'preco': 'price',
+        'quantidade': 'amount',
+        'nome': 'name',
+        'idade': 'age'
+    }
+
+    # Standardize the names
+    print('Translating columns...')
+    df.columns = [
+        corrections[col] if col in corrections else
+        GoogleTranslator(source='auto', target='en').translate(col) for col in df.columns
+    ]
+    # ========================================================================
+    # 2. SPECIFIC TREATMENTS - Only run if the columns exists
+    # =======================================================================
+
+    # if base for clients/RH and have age:
+    if 'age' in df.columns:
+        df['age'] = pd.to_numeric(df['age'], errors='coerce')
+        df['age'] = df['age'].fillna(df['age'].median())
+        print(".  - Columns 'age' treated with median")
+
+    # if base for sales/e-commerce and have price and amount:
+    if 'price' in df.columns and 'amount' in df.columns:
+        df['price'] = pd.to_numeric(df['price'], errors='coerce')
+        df['amount'] = pd.to_numeric(df['amount'], errors='coerce')
+        df['total_revenue'] = df['price'] * df['amount']
+        print(".  - Columns 'total_revenue' calculate automatically.")
+
+    # if base there are any columns of DATA:
+    if 'data_sale' in df.columns:
+        df['time_sale'] = pd.to_datetime(df['time_sale'], errors='coerce')
+        print(".  - Columns 'time_sale' converted for to format correct. ")
+
+    # ===============================================================
+    # 3. FINAL TREATMENT OF RESIDUAL NULLS
+    # ===============================================================
+
+    # Any remaining empty space in text columns becomes "not provided."
+    df = df.fillna('Not provided')
 
     # Save Step
     print(f"Salving cleaning data in: {exit_path}")
